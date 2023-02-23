cbd9376cbdcb1fc2e96cab6f6214b561d2ea5cc0
BurstLime <burstlime@gmail.com>
2023-02-24 00:37:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Random Trifecta/Random Trifecta.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Random_Trifecta
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list = new System.Windows.Forms.CheckedListBox();
+            this.box1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // list
+            // 
+            this.list.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.list.FormattingEnabled = true;
+            this.list.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18"});
+            this.list.Location = new System.Drawing.Point(12, 12);
+            this.list.Name = "list";
+            this.list.Size = new System.Drawing.Size(103, 323);
+            this.list.TabIndex = 0;
+            // 
+            // box1
+            // 
+            this.box1.Location = new System.Drawing.Point(171, 44);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(416, 88);
+            this.box1.TabIndex = 1;
+            this.box1.TabStop = false;
+            this.box1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.box1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "固定";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(428, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 349);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.list);
+            this.Name = "main";
+            this.ShowIcon = false;
+            this.Text = "Random Trifecta";
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox list;
+        public System.Windows.Forms.PictureBox box1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Random Trifecta/Random Trifecta.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Trifecta
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            //設定
+            int interval = 70;
+            int bold = 1;
+
+            //ボックスの作成
+            Bitmap canvas = new Bitmap(box1.Width, box1.Height);
+            Graphics g = Graphics.FromImage(canvas);
+            Pen p = new Pen(Color.Black, bold);
+            Rectangle[] box = { new Rectangle(0, 0, box1.Height-bold, box1.Height-bold),
+                new Rectangle(box1.Height+interval, 0, box1.Height-bold, box1.Height-bold),
+                new Rectangle((box1.Height+interval)*2, 0, box1.Height-bold, box1.Height-bold)};
+            g.DrawRectangles(p, box);
+
+            //線の作成
+            g.DrawLine(Pens.Black, (float)(box1.Height + (interval * 0.25)), box1.Height/2, (float)(box1.Height + (interval * 0.75)), box1.Height / 2);
+            g.DrawLine(Pens.Black, (float)(box1.Height*2 + (interval * 1.25)), box1.Height/2, (float)(box1.Height*2 + (interval * 1.75)), box1.Height / 2);
+
+            p.Dispose();
+            g.Dispose();
+            box1.Image = canvas;
+        }
+
+        private void box1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            //設定
+            int interval = 70;
+            int bold = 1;
+
+            if ((0 <= e.X && e.X <= box1.Height - bold )&&( 0 <= e.Y && e.Y <= box1.Height - bold)) //1
+            {
+                
+            }else if ((box1.Height + interval <= e.X && e.X <= box1.Height*2 + interval) && (0 <= e.Y && e.Y <= box1.Height - bold)) //2
+            {
+                button1.Enabled = false;
+            }else if (((box1.Height + interval)*2 <= e.X && e.X <= box1.Height*3 + interval*2) && (0 <= e.Y && e.Y <= box1.Height - bold)) //3
+            {
+                button2.Enabled = false;
+            }
+        }
+    }
+}
